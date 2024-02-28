36fa38a0899a5db74f69c03cfbb9526890b54d8d
Riadh <22998716+KiKoS0@users.noreply.github.com>
2024-02-28 11:25:26 +0100

    Adjust ktor test server to the InngestFunction change

---
+++ inngest-test-server/src/main/kotlin/com/inngest/testserver/UserSignupFunction.kt
@@ -0,0 +1,48 @@
+package com.inngest.testserver
+
+import com.inngest.*
+import java.time.Duration
+
+@FunctionConfig(id = "fn-id-slug", name = "My function!")
+@FunctionEventTrigger(event = "user-signup")
+class UserSignupFunction : InngestFunction() {
+    override fun execute(
+        ctx: FunctionContext,
+        step: Step,
+    ): HashMap<String, String> {
+        val x = 10
+
+        println("-> handler called " + ctx.event.name)
+
+        val y =
+            step.run<Int>("add-ten") { ->
+                x + 10
+            }
+
+        val res =
+            step.run<Result>("cast-to-type-add-ten") { ->
+                println("-> running step 1!! " + x)
+                // throw Exception("An error!")
+                Result(
+                    sum = y + 10,
+                )
+            }
+        println("res" + res)
+
+        step.waitForEvent("wait-for-hello", "hello", "10m", "event.data.hello == async.data.hello")
+
+        val add: Int =
+            step.run<Int>("add-one-hundred") {
+                println("-> running step 2 :) " + res?.sum)
+                res.sum + 100
+            }
+
+        step.sleep("wait-one-sec", Duration.ofSeconds(2))
+
+        step.run("last-step") { res.sum.times(add) ?: 0 }
+
+        step.sendEvent("followup-event-id", InngestEvent(FOLLOW_UP_EVENT_NAME, data = hashMapOf("hello" to "world")))
+
+        return hashMapOf("message" to "cool - this finished running")
+    }
+}
