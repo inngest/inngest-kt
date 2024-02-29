b0f78498441d7583a037ddc2183522a053ae3782
Darwin D Wu <darwin67@users.noreply.github.com>
2024-02-29 15:15:28 -0800

    copy gradle settings from inngest

--- inngest-spring-boot-adapter/build.gradle.kts
+++ inngest-spring-boot-adapter/build.gradle.kts
@@ -1,27 +1,25 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 group = "com.inngest"
 description = "Spring Boot adapter for Inngest SDK"
-version = "0.0.1"
+version = file("VERSION").readText().trim()
 
 plugins {
-    `java-library`
+    id("java-library")
     id("maven-publish")
+    id("signing")
     id("io.spring.dependency-management") version "1.1.4"
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
+    withJavadocJar()
+    withSourcesJar()
 }
 
 repositories {
     mavenCentral()
-
-    maven {
-        url = uri("https://maven.pkg.github.com/inngest/inngest-kt")
-        credentials {
-            username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
-            password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
-        }
-    }
 }
 
 dependencies {
@@ -41,22 +39,135 @@ dependencyManagement {
 
 publishing {
     repositories {
+        // NOTE: Does not work: https://central.sonatype.org/publish/publish-portal-gradle/
+        // maven {
+        //     name = "OSSRH"
+        //     url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+        //     credentials {
+        //         username = System.getenv("MAVEN_USERNAME")
+        //         password = System.getenv("MAVEN_PASSWORD")
+        //     }
+        // }
+
+        // NOTE: create a local repo and bundle this to upload it to Maven Central for now
+        maven {
+            // local repo
+            val releasesRepoUrl = uri(layout.buildDirectory.dir("repos/releases"))
+            val snapshotsRepoUrl = uri(layout.buildDirectory.dir("repos/snapshots"))
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+        }
+
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/inngest/inngest-kt")
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
             }
         }
     }
     publications {
-        register<MavenPublication>("gpr") {
+        register<MavenPublication>("inngest") {
             from(components["java"])
+
+            pom {
+                name.set(project.name)
+                description.set("Inngest SDK for Kotlin/Java")
+                url.set("https://github.com/inngest/inngest-kt")
+                inceptionYear.set("2024")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("eng")
+                        name.set("Inngest Engineering")
+                        email.set("eng@inngest.com")
+                    }
+                }
+
+                scm {
+                    url.set("https://github.com/inngest/inngest-kt")
+                    connection.set("scm:git:https://github.com/inngest/inngest-kt.git")
+                    developerConnection.set("scm:git:git@github.com:inngest/inngest-kt.git")
+                }
+            }
         }
+    }
+}
+
+signing {
+    val signingKey = System.getenv("GPG_SIGNING_KEY")
+    val signingPasswd = System.getenv("GPG_SIGNING_KEY_PASSWORD")
+    useInMemoryPgpKeys(signingKey, signingPasswd)
+    sign(publishing.publications)
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+            mapOf(
+                "Implementation-Title" to project.name,
+                "Implementation-Version" to project.version,
+            ),
+        )
+    }
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
     }
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+
+    testLogging {
+        events =
+            setOf(
+                TestLogEvent.FAILED,
+                TestLogEvent.PASSED,
+                TestLogEvent.SKIPPED,
+            )
+
+        exceptionFormat = TestExceptionFormat.FULL
+        showStandardStreams = true
+        showExceptions = true
+        showCauses = true
+        showStackTraces = true
+
+        // set options for log level DEBUG and INFO
+        debug {
+            events =
+                setOf(
+                    TestLogEvent.STARTED,
+                    TestLogEvent.FAILED,
+                    TestLogEvent.PASSED,
+                    TestLogEvent.SKIPPED,
+                    TestLogEvent.STANDARD_ERROR,
+                    TestLogEvent.STANDARD_OUT,
+                )
+
+            exceptionFormat = TestExceptionFormat.FULL
+        }
+
+        info.events = debug.events
+        info.exceptionFormat = debug.exceptionFormat
+
+        afterSuite(
+            KotlinClosure2({ desc: TestDescriptor, result: TestResult ->
+                if (desc.parent == null) { // will match the outermost suite
+                    println(
+                        "Results: ${result.resultType} (${result.testCount} tests, ${result.successfulTestCount} successes, ${result.failedTestCount} failures, ${result.skippedTestCount} skipped)",
+                    )
+                }
+            }),
+        )
+    }
 }
