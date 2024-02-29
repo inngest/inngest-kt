a3112312b0ff28ac5fced18f0d1ca3e5486dc2e6
Darwin <5746693+darwin67@users.noreply.github.com>
2024-02-29 14:49:03 -0800

    Setup workflow to upload to Maven central (#49)
    Update the SDK core's `build.gradle.kts` to publish to a local Maven repo (which is just basically a file path), and bundle that to send it over to Maven Central's publisher API.
    
    The new Maven Central don't have integration with gradle atm, so unfortunately, we'll just have to automate it in a way simulating a manual upload.
    https://central.sonatype.org/publish/publish-portal-api/
    
    The `Release` github workflow is set to trigger when there are changes made to each project's `VERSION` file. Which assumes that we'll want to publish a new version.
    
    ---------
    Co-authored-by: Darwin D Wu <darwin67@users.noreply.github.com>

--- inngest/build.gradle.kts
+++ inngest/build.gradle.kts
@@ -3,16 +3,20 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 group = "com.inngest"
 description = "Inngest SDK"
-version = "0.0.2"
+version = file("VERSION").readText().trim()
 
 plugins {
+    id("java-library")
     id("maven-publish")
+    id("signing")
     id("org.jetbrains.kotlin.jvm") version "1.9.10"
 }
 
 // TODO - Move this to share conventions gradle file
 java {
     toolchain { languageVersion.set(JavaLanguageVersion.of(8)) }
+    withJavadocJar()
+    withSourcesJar()
 }
 
 repositories {
@@ -34,29 +38,73 @@ dependencies {
 
 publishing {
     repositories {
+        // NOTE: Does not work: https://central.sonatype.org/publish/publish-portal-gradle/
         // maven {
         //     name = "OSSRH"
-        //     url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+        //     url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
         //     credentials {
         //         username = System.getenv("MAVEN_USERNAME")
         //         password = System.getenv("MAVEN_PASSWORD")
         //     }
         // }
 
+        // NOTE: create a local repo and bundle this to upload it to Maven Central for now
+        maven {
+            // local repo
+            val releasesRepoUrl = uri(layout.buildDirectory.dir("repos/releases"))
+            val snapshotsRepoUrl = uri(layout.buildDirectory.dir("repos/snapshots"))
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+        }
+
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/inngest/inngest-kt")
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
             }
         }
     }
     publications {
-        register<MavenPublication>("gpr") {
+        register<MavenPublication>("inngest") {
             from(components["java"])
+
+            pom {
+                name.set(project.name)
+                description.set("Inngest SDK for Kotlin/Java")
+                url.set("https://github.com/inngest/inngest-kt")
+                inceptionYear.set("2024")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("eng")
+                        name.set("Inngest Engineering")
+                        email.set("eng@inngest.com")
+                    }
+                }
+
+                scm {
+                    url.set("https://github.com/inngest/inngest-kt")
+                    connection.set("scm:git:https://github.com/inngest/inngest-kt.git")
+                    developerConnection.set("scm:git:git@github.com:inngest/inngest-kt.git")
+                }
+            }
         }
     }
+}
+
+signing {
+    val signingKey = System.getenv("GPG_SIGNING_KEY")
+    val signingPasswd = System.getenv("GPG_SIGNING_KEY_PASSWORD")
+    useInMemoryPgpKeys(signingKey, signingPasswd)
+    sign(publishing.publications)
 }
 
 tasks.jar {
@@ -67,6 +115,12 @@ tasks.jar {
                 "Implementation-Version" to project.version,
             ),
         )
+    }
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
     }
 }
 
