e5484410fc57d9cc660d02fb5161355bc1fa2fa2
Darwin D Wu <darwin67@users.noreply.github.com>
2024-02-28 18:01:00 -0800

    update code style settings due to deprecation

--- inngest-spring-boot-adapter/build.gradle.kts
+++ inngest-spring-boot-adapter/build.gradle.kts
@@ -1,10 +1,12 @@
+group = "com.inngest"
+description = "Spring Boot adapter for Inngest SDK"
+version = "0.0.1"
+
 plugins {
     `java-library`
+    id("maven-publish")
     id("io.spring.dependency-management") version "1.1.4"
 }
-
-group = "com.inngest"
-version = "0.0.2-SNAPSHOT2"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
@@ -25,6 +27,24 @@ dependencyManagement {
     imports {
         mavenBom("org.springframework.boot:spring-boot-dependencies:2.7.18") {
             bomProperty("kotlin.version", "1.9.10")
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/inngest/inngest-kt")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
         }
     }
 }
