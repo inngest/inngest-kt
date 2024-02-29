13d36b033837473865936b31a350039a01681108
Darwin D Wu <darwin67@users.noreply.github.com>
2024-02-28 18:48:49 -0800

    update adapter dependency

--- inngest-spring-boot-demo/build.gradle.kts
+++ inngest-spring-boot-demo/build.gradle.kts
@@ -28,7 +28,7 @@ repositories {
 }
 
 dependencies {
-    implementation(project(":inngest-spring-boot-adapter"))
+    implementation("com.inngest:inngest-spring-boot-adapter:0.0.+")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
