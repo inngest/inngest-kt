df7c2c43059f1dddb23dfda5bb4207f44b9ed826
Dan Farrelly <daniel.j.farrelly@gmail.com>
2024-07-17 10:01:28 -0700

    Add fn

---
+++ inngest-test-server/src/main/kotlin/com/inngest/testserver/RestoreFromGlacier.kt
@@ -0,0 +1,45 @@
+package com.inngest.testserver
+
+import com.inngest.*
+import java.time.Duration
+
+@FunctionConfig(id = "RestoreFromGlacier", name = "RestoreFromGlacier")
+@FunctionEventTrigger(event = "delivery/restore.requested")
+class RestoreFromGlacier : InngestFunction() {
+    override fun execute(
+        ctx: FunctionContext,
+        step: Step,
+    ): LinkedHashMap<String, Any> {
+
+        step.run("restore") {
+            if (!isRestoredFromGlacier(0)) {
+                restoreFromGlacier()
+            }
+        }
+        var i = 0
+        while (i < 6) {
+            val isRestored = step.run(String.format("check-status-%d", i)) {
+                isRestoredFromGlacier(i)
+            }
+            if (isRestored) {
+                return linkedMapOf("restored" to true)
+            }
+            step.sleep(String.format("wait-for-restore-%d", i), Duration.ofSeconds(5))
+            i++
+        }
+
+//        throw NonRetriableError("Could not restore")
+        return linkedMapOf("restored" to false)
+    }
+
+    fun isRestoredFromGlacier(temp: Int): Boolean {
+        if (temp > 2) {
+            return true
+        }
+        return false;
+    }
+
+    fun restoreFromGlacier(): String {
+        return "FILES_RESTORED"
+    }
+}
