bf5da4fe94b802426c8f9c2cba7c19908f32c767
albertchae <217050+albertchae@users.noreply.github.com>
2024-09-09 21:47:38 -0700

    Add idempotency configuration (#80)
    Copied doc from
    https://github.com/inngest/inngest/blob/6594ccb692121bfe62b11676fe002416df13300b/docs/SDK_SPEC.md?plain=1#L534-L538
    
    https://www.inngest.com/docs/guides/handling-idempotency#at-the-function-level-the-consumer
    
    The integration test validates idempotency by
    - checking an observable side effect, in this case incrementing a static
      counter variable
    - check that the second run of the same idempotency was ignored by the
      dev server

---
+++ inngest-spring-boot-demo/src/test/java/com/inngest/springbootdemo/IdempotentFunctionIntegrationTest.java
@@ -0,0 +1,51 @@
+package com.inngest.springbootdemo;
+
+import com.inngest.Inngest;
+import com.inngest.springbootdemo.testfunctions.IdempotentFunction;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Collections;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@IntegrationTest
+@Execution(ExecutionMode.CONCURRENT)
+class IdempotentFunctionIntegrationTest {
+    @Autowired
+    private DevServerComponent devServer;
+
+    @Autowired
+    private Inngest client;
+
+    @Test
+    void testIdempotencyKey() throws Exception {
+        Map dataPayload = Collections.singletonMap("companyId", 42);
+        String eventWithIdempotencyKey = InngestFunctionTestHelpers.sendEvent(client, "test/idempotent", dataPayload).getIds()[0];
+        String eventWithSameIdempotencyKey = InngestFunctionTestHelpers.sendEvent(client, "test/idempotent", dataPayload).getIds()[0];
+
+        Thread.sleep(2000);
+
+        // With the same idempotency key, only one of the events should have run
+        RunEntry<Object> firstRun = devServer.runsByEvent(eventWithIdempotencyKey).first();
+        assertEquals(0, devServer.runsByEvent(eventWithSameIdempotencyKey).data.length);
+        assertEquals("Completed", firstRun.getStatus());
+
+        // This would be 2 if the function was not idempotent
+        assertEquals(1, IdempotentFunction.getCounter());
+
+
+        Map differentDataPayload = Collections.singletonMap("companyId", 43);
+        String eventWithDifferentIdempotencyKey = InngestFunctionTestHelpers.sendEvent(client, "test/idempotent", differentDataPayload).getIds()[0];
+
+        Thread.sleep(2000);
+
+        // Event with a different idempotency key will run once
+        RunEntry<Object> otherRun = devServer.runsByEvent(eventWithDifferentIdempotencyKey).first();
+        assertEquals("Completed", otherRun.getStatus());
+        assertEquals(2, IdempotentFunction.getCounter());
+    }
+}
