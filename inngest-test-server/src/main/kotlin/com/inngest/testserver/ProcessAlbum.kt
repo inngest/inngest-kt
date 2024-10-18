615986a18b881313b4a9facca40607c35ba6db05
KiKoS0 <22998716+KiKoS0@users.noreply.github.com>
2024-10-18 09:07:24 +0900

    Reproduction of UnrecognizedPropertyException

--- inngest-test-server/src/main/kotlin/com/inngest/testserver/ProcessAlbum.kt
+++ inngest-test-server/src/main/kotlin/com/inngest/testserver/ProcessAlbum.kt
@@ -6,35 +6,35 @@ import java.time.Duration
 /**
  * A demo function that accepts an event in a batch and invokes a child function
  */
+class PoolItemNotAvailableException(
+    message: String,
+    cause: Throwable? = null,
+) : Exception(message, cause)
+
 class ProcessAlbum : InngestFunction() {
     override fun config(builder: InngestFunctionConfigBuilder): InngestFunctionConfigBuilder =
         builder
             .id("ProcessAlbum")
             .name("Process Album!")
             .triggerEvent("delivery/process.requested")
-            .batchEvents(30, Duration.ofSeconds(10))
+            .retries(0)
 
     override fun execute(
         ctx: FunctionContext,
         step: Step,
     ): LinkedHashMap<String, Any> {
-//        val list = ctx.events.map { e -> e.data.get("something") }
-//        println(list);
+        try {
+            step.run<String>("process-album") {
+                throw PoolItemNotAvailableException("pool1", null)
+                "result"
+            }
 
-        for (evt in ctx.events) {
-//            println(evt);
-            // NOTE - App ID is set on the serve level
-            val res =
-                step.invoke<Map<String, Any>>(
-                    "restore-album-${evt.data["albumId"]}",
-                    "ktor-dev",
-                    "RestoreFromGlacier",
-                    evt.data,
-                    null,
-                )
-            println(res["restored"])
+        } catch (e: Exception) { // throws UnrecognizedPropertyException
+//        } catch (e: StepError) { // this should successfully deserialize the exception
+            step.run("handle-error") {
+                e
+            }
         }
-
         return linkedMapOf("hello" to true)
     }
 }
