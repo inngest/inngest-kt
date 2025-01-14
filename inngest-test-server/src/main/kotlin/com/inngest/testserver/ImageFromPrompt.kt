316ac72f1d5fbeffb1e719cebe71b112a4363502
Riadh <22998716+KiKoS0@users.noreply.github.com>
2025-01-14 13:43:52 +0100

    POC to trigger the incorrect state type bug

--- inngest-test-server/src/main/kotlin/com/inngest/testserver/ImageFromPrompt.kt
+++ inngest-test-server/src/main/kotlin/com/inngest/testserver/ImageFromPrompt.kt
@@ -12,36 +12,8 @@ class ImageFromPrompt : InngestFunction() {
     override fun execute(
         ctx: FunctionContext,
         step: Step,
-    ): String {
-        val imageURL =
-            try {
-                step.run("generate-image-dall-e") {
-                    // Call the DALL-E model to generate an image
-                    throw Exception("Failed to generate image")
-
-                    "example.com/image-dall-e.jpg"
-                }
-            } catch (e: StepError) {
-                // Fall back to a different image generation model
-                step.run("generate-image-midjourney") {
-                    // Call the MidJourney model to generate an image
-                    "example.com/image-midjourney.jpg"
-                }
-            }
-
-        try {
-            step.invoke<Map<String, Any>>(
-                "push-to-slack-channel",
-                "ktor-dev",
-                "PushToSlackChannel",
-                mapOf("image" to imageURL),
-                null,
-            )
-        } catch (e: StepError) {
-            // Pushing to Slack is not critical, so we can ignore the error, log it
-            // or handle it in some other way.
+    ): String? =
+        step.run("generate-image-dall-e") {
+            null as String?
         }
-
-        return imageURL
-    }
 }
